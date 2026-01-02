1ae587f69bc37a3faf4fae0aa35267dc91e8bdca
Stuart Blackler <Im5tu@users.noreply.github.com>
2026-01-02 21:42:40 +0000

    Initial lambda module (#1)

---
+++ iam.tf
@@ -0,0 +1,41 @@
+module "iam_this" {
+  source = "git::https://github.com/im5tu/opentofu-aws-iam-role.git?ref=847ba166bc7c2e147573ff8d222487ca22347e15"
+
+  name_prefix = var.name
+  tags = merge(var.tags, {
+    Name = var.name
+  })
+  assume_role_services = ["lambda.amazonaws.com"]
+  external_attachment_arns = concat(
+    length(var.vpc_subnet_ids) > 0 ? ["arn:aws:iam::aws:policy/service-role/AWSLambdaENIManagementAccess"] : [],
+    var.iam_policy_attachments
+  )
+  policies = merge(var.iam_inline_policies, {
+    "cloudwatch"  = data.aws_iam_policy_document.cloudwatch.json
+    "dead_letter" = data.aws_iam_policy_document.dead_letter.json
+  })
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  statement {
+    sid    = "AllowCloudwatchLogsAccess"
+    effect = "Allow"
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogStream"
+    ]
+    resources = [aws_cloudwatch_log_group.this.arn, "${aws_cloudwatch_log_group.this.arn}:*"]
+  }
+}
+
+data "aws_iam_policy_document" "dead_letter" {
+  statement {
+    sid    = "AllowDeadLetterAccess"
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage",
+      "sns:Publish"
+    ]
+    resources = [var.dead_letter_target_arn]
+  }
+}
